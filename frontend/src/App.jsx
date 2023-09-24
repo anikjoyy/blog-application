dc60e025b3374cc82af3c0237c581461c5a2d2ce
anikjoyy <anikjoy.cse@gmail.com>
2023-09-24 19:19:44 +0600

    navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <>
-      <h1 className='text-blue-500'>Hello Clone!</h1>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
